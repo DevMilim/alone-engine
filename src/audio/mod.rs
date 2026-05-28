e003923c33eede296b6a1b7f07b2ba2f417f4a6d
DevMilim <devmilim@gmail.com>
2026-05-28 08:13:01 -0300

    update

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,1 +1,14 @@
+use rodio::{DeviceSinkBuilder, MixerDeviceSink};
 
+pub struct Audio {
+    sink: MixerDeviceSink,
+}
+
+impl Audio {
+    fn new() -> Self {
+        let stream_handle = DeviceSinkBuilder::open_default_sink().unwrap();
+        Self {
+            sink: stream_handle,
+        }
+    }
+}
