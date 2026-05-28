95c5db401937dc780e33bb417ad3c897c58aa2c2
DevMilim <devmilim@gmail.com>
2026-05-28 11:04:34 -0300

    Adicionando componente de timer

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,9 +1,17 @@
+mod animated_sprite;
+mod audio;
 mod body;
 mod camera;
 mod collision;
 mod rigid_body;
+mod tilemap;
+mod timer;
 
+pub use animated_sprite::*;
+pub use audio::*;
 pub use body::*;
 pub use camera::*;
 pub use collision::*;
 pub use rigid_body::*;
+pub use tilemap::*;
+pub use timer::*;
---
+++ src/components/timer.rs
@@ -0,0 +1,61 @@
+use std::{
+    any::Any,
+    time::{Duration, Instant},
+};
+
+use crate::Component;
+
+pub enum TimerEvent {
+    Timeout,
+}
+
+pub struct Timer {
+    instant: Option<Instant>,
+    duration: Duration,
+    event: Option<Box<dyn Fn() -> Box<dyn Any + 'static>>>,
+    repeat: bool,
+}
+
+impl Timer {
+    pub fn new() -> Self {
+        Self {
+            instant: None,
+            duration: Duration::from_secs(1),
+            event: None,
+            repeat: true,
+        }
+    }
+    fn now(&mut self) {
+        self.instant = Some(Instant::now());
+    }
+    pub fn set_event<T: Clone + 'static>(&mut self, event: T) {
+        self.event = Some(Box::new(move || Box::new(event.clone())));
+    }
+    pub fn start(&mut self, duration: Duration, repeat: bool) {
+        self.duration = duration;
+        self.repeat = repeat;
+        self.instant = Some(Instant::now())
+    }
+    pub fn stop(&mut self) {
+        self.instant = None
+    }
+}
+
+impl Component for Timer {
+    fn update(&mut self, ctx: &mut impl crate::EngineApi, base: &mut crate::Base, _delta: f32) {
+        if let Some(instant) = self.instant {
+            if instant.elapsed() >= self.duration {
+                if self.repeat {
+                    self.instant = Some(instant + self.duration);
+                } else {
+                    self.stop();
+                }
+                if let Some(event) = &self.event {
+                    ctx.send(base.id, event());
+                } else {
+                    ctx.emit_targeted(base.id, TimerEvent::Timeout);
+                }
+            }
+        }
+    }
+}
