52452569492901f1c90be979c906e9866975811c
DevMilim <devmilim@gmail.com>
2026-05-31 11:39:51 -0300

    atualizando a macro para assinaturas mais simples como #[base]

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,27 +1,107 @@
-use alone_engine::{Base, GameObject, GameObjectBase, Scene, Vector2, run_application};
+use std::time::Duration;
 
-use crate::player::Player;
+use alone_engine::{
+    Base, Component, EngineApi, GameObject, GameObjectBase, KeyCode, Scene, SpawnEvent, Tilemap,
+    Timer, Vector2, run_application,
+};
+#[derive(GameObject)]
+struct Bullet {
+    #[base]
+    base: Base,
+    #[component]
+    timer: Timer,
+}
 
-mod player;
+impl Bullet {
+    pub fn new() -> Self {
+        Bullet {
+            base: Base::empty(),
+            timer: Timer::new(),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum BulletEvent {
+    Free,
+}
+
+impl GameObject for Bullet {
+    type Message = BulletEvent;
+    fn start(&mut self, _ctx: &mut impl EngineApi) {
+        println!("Bullet");
+        self.timer.start_timer(Duration::from_secs(2), false);
+        self.timer.set_event(BulletEvent::Free);
+    }
+    fn on_message(&mut self, _ctx: &mut impl EngineApi, msg: &Self::Message) {
+        match msg {
+            BulletEvent::Free => {
+                println!("Exit");
+                self.base.queue_free();
+            }
+        }
+    }
+    fn destroy(&mut self, _ctx: &mut impl EngineApi) {
+        println!("destroy chamado")
+    }
+}
 
 #[derive(GameObject)]
-pub struct MainScene {
-    #[game(base)]
+pub struct Player {
+    #[base]
     base: Base,
-    #[game(object)]
+}
+
+impl Player {
+    pub fn new(position: Vector2) -> Self {
+        Self {
+            base: Base::new(position),
+        }
+    }
+}
+
+impl GameObject for Player {
+    type Message = ();
+    fn update(&mut self, ctx: &mut impl EngineApi, _delta: f32) {
+        if ctx.is_key_just_pressed(KeyCode::Space) {
+            ctx.spawn(Bullet::new());
+        }
+    }
+}
+
+#[derive(GameObject)]
+#[game(subscribe(spawn_bullet: SpawnEvent<Bullet>))]
+pub struct MainScene {
+    #[base]
+    base: Base,
+    #[object]
     player: Player,
+    #[component]
+    tilemap: Option<Tilemap>,
+    #[object]
+    bullets: Vec<Bullet>,
 }
 impl MainScene {
     pub fn new() -> Self {
         Self {
             base: Base::default(),
             player: Player::new(Vector2::ZERO),
+            tilemap: None,
+            bullets: Vec::new(),
         }
+    }
+    fn spawn_bullet(&mut self, _ctx: &mut impl EngineApi, event: &SpawnEvent<Bullet>) {
+        self.bullets
+            .push(event.take().expect("Erro ao spawnar bullet"));
     }
 }
 
 impl GameObject for MainScene {
     type Message = ();
+    fn start(&mut self, _ctx: &mut impl alone_engine::EngineApi) {
+        //self.tilemap =
+        //    Some(Tilemap::from_ldtk_file(ctx, "assets/tilemap.ldtk", "Level_0").unwrap());
+    }
 }
 
 #[derive(Scene)]
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,17 +3,38 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{Ident, Meta, Path, Token, Type, parse::Parse, punctuated::Punctuated};
 
-#[derive(Debug, FromField)]
-#[darling(attributes(game))]
+#[derive(Debug)]
 struct GameField {
     ident: Option<syn::Ident>,
     ty: syn::Type,
-    #[darling(default)]
     base: bool,
-    #[darling(default)]
     component: bool,
-    #[darling(default)]
     object: bool,
+}
+
+impl FromField for GameField {
+    fn from_field(field: &syn::Field) -> darling::Result<Self> {
+        let mut base = false;
+        let mut component = false;
+        let mut object = false;
+
+        for attr in &field.attrs {
+            if attr.path().is_ident("base") {
+                base = true;
+            } else if attr.path().is_ident("component") {
+                component = true;
+            } else if attr.path().is_ident("object") {
+                object = true
+            }
+        }
+        Ok(GameField {
+            ident: field.ident.clone(),
+            ty: field.ty.clone(),
+            base,
+            component,
+            object,
+        })
+    }
 }
 
 #[derive(Debug, FromDeriveInput)]
@@ -49,18 +70,18 @@ impl Parse for Subscription {
 fn parse_subscriptions(meta: &Meta) -> Result<Vec<Subscription>, Error> {
     let list = match meta {
         Meta::List(list) => list,
-        _ => return Err(Error::custom("Use formato: subscribe(metodo: Tipo)")),
+        _ => return Err(Error::custom("Use o formato: subscribe(metodo: Tipo)")),
     };
 
     let parser = Punctuated::<Subscription, Token![,]>::parse_terminated;
-
     let subs = list
         .parse_args_with(parser)
         .map_err(|e| Error::custom(e.to_string()).with_span(meta))?;
+
     Ok(subs.into_iter().collect())
 }
 
-#[proc_macro_derive(GameObject, attributes(game))]
+#[proc_macro_derive(GameObject, attributes(base, component, object, game))]
 pub fn scene_tree(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     let receiver = match GameReceiver::from_derive_input(&input) {
@@ -68,52 +89,45 @@ pub fn scene_tree(input: TokenStream) -> TokenStream {
         Err(e) => return e.write_errors().into(),
     };
 
-    let subscribe_block = if !receiver.subscribe.is_empty() {
+    // 1. Otimização dos blocos de eventos (gerados condicionalmente para evitar overhead)
+    let subscribe_block = (!receiver.subscribe.is_empty()).then(|| {
         let arms = receiver.subscribe.iter().map(|sub| {
             let event_ty = &sub.event_type;
             let handler_ident = &sub.handler;
-
             quote! {
-                if let Some(payload) = any_event.downcast_ref::<#event_ty>(){
+                if let Some(payload) = any_event.downcast_ref::<#event_ty>() {
                     self.#handler_ident(ctx, payload);
                 }
             }
         });
         quote! {
-            if let ::alone_engine::GlobalEvent::Broadcast(any_event) = event{
+            if let ::alone_engine::GlobalEvent::Broadcast(any_event) = event {
                 #(#arms)*
             }
         }
-    } else {
-        quote! {}
-    };
+    });
 
-    let connect_block = if !receiver.connect.is_empty() {
+    let connect_block = (!receiver.connect.is_empty()).then(|| {
         let arms = receiver.connect.iter().map(|sub| {
             let event_ty = &sub.event_type;
             let handler_ident = &sub.handler;
-
             quote! {
-                if let Some(payload) = any_event.downcast_ref::<#event_ty>(){
+                if let Some(payload) = any_event.downcast_ref::<#event_ty>() {
                     self.#handler_ident(ctx, payload);
                 }
             }
         });
         quote! {
-            if let ::alone_engine::GlobalEvent::Targeted(id, any_event) = event{
-                if &self.base().id == id{
+            if let ::alone_engine::GlobalEvent::Targeted(id, any_event) = event {
+                if &self.base().id == id {
                     #(#arms)*
                     return;
                 }
             }
-
         }
-    } else {
-        quote! {}
-    };
+    });
 
     let struct_name = &receiver.ident;
-
     let fields = receiver.data.take_struct().unwrap();
 
     let mut base_field = None;
@@ -124,6 +138,7 @@ pub fn scene_tree(input: TokenStream) -> TokenStream {
     for field in fields.fields {
         let ident = field.ident.as_ref().unwrap();
         let ty = &field.ty;
+
         if field.base {
             if base_field.is_some() {
                 return syn::Error::new_spanned(
@@ -136,31 +151,45 @@ pub fn scene_tree(input: TokenStream) -> TokenStream {
             if type_is_base(ty) {
                 base_field = Some(ident.clone());
             } else {
-                return syn::Error::new_spanned(ty, "Precisa ser do tipo Transform2D")
+                return syn::Error::new_spanned(ty, "O campo base precisa ser do tipo Base")
                     .to_compile_error()
                     .into();
             }
         } else if field.component {
             component_fields.push(ident.clone());
-            bounds.push(quote! {#ty: ::alone_engine::Component});
+            bounds.push(quote! { #ty: ::alone_engine::Component });
         } else if field.object {
             object_fields.push(ident.clone());
             bounds.push(
-                quote! {#ty: ::alone_engine::GameObject + ::alone_engine::GameObjectDispatch},
+                quote! { #ty: ::alone_engine::GameObject + ::alone_engine::GameObjectDispatch },
             );
         }
     }
+
+    let base_field = match base_field {
+        Some(field) => field,
+        None => {
+            return syn::Error::new_spanned(
+                struct_name,
+                "Nenhum campo foi marcado com #[game(base)]",
+            )
+            .to_compile_error()
+            .into();
+        }
+    };
+
     let apply_transform = quote! {
         let inherit = !self.#base_field.top_level;
         self.#base_field.transform.apply_parent(&parent_base.transform, inherit);
     };
-    let (impl_generics, ty_generics, where_clause) = receiver.generics.split_for_impl();
 
+    let (impl_generics, ty_generics, where_clause) = receiver.generics.split_for_impl();
     let where_tokens = if let Some(wc) = where_clause {
-        quote! {#wc,  Self: ::alone_engine::GameObject, #(#bounds),*}
+        quote! { #wc, Self: ::alone_engine::GameObject, #(#bounds),* }
     } else {
-        quote! {where  Self: ::alone_engine::GameObject, #(#bounds),*}
+        quote! { where Self: ::alone_engine::GameObject, #(#bounds),* }
     };
+
     quote! {
         impl #impl_generics ::alone_engine::GameObjectBase for #struct_name #ty_generics {
             fn base(&self) -> &::alone_engine::Base {
@@ -171,95 +200,93 @@ pub fn scene_tree(input: TokenStream) -> TokenStream {
                 &mut self.#base_field
             }
         }
+
         impl #impl_generics ::alone_engine::GameObjectDispatch for #struct_name #ty_generics #where_tokens {
-            fn is_pending_removal(&self) -> bool{
+            fn is_pending_removal(&self) -> bool {
                 self.base().pending_removal
             }
+
             fn dispatch_start(&mut self, ctx: &mut impl ::alone_engine::EngineApi, parent_base: &::alone_engine::Base) {
-                if self.is_started(){
+                if self.is_started() {
                     return;
                 }
                 self.start(ctx);
                 #(self.#component_fields.start(ctx, &mut self.#base_field);)*
-                #( self.#object_fields.dispatch_start(ctx, &self.#base_field); )*
+                #(self.#object_fields.dispatch_start(ctx, &self.#base_field);)*
             }
-            fn dispatch_message(&mut self, ctx: &mut impl ::alone_engine::EngineApi){
-                {
-                    if ctx.mail_box_is_empty(){
-                        return;
-                    }
+
+            fn dispatch_message(&mut self, ctx: &mut impl ::alone_engine::EngineApi) {
+                if ctx.mail_box_is_empty() {
+                    return;
                 }
-                {
-                    let mailbox = ctx.mailbox();
-                    if let Some(msgs) = mailbox.remove(&self.base().id){
-                        for msg in msgs {
-                            if let Some(message) = msg.downcast_ref::<<Self as ::alone_engine::GameObject>::Message>(){
-                                self.on_message(ctx, message)
-                            } else{
-                                println!("Tipo de evento incompativel recebido");
-                            }
+
+                let mailbox = ctx.mailbox();
+                if let Some(msgs) = mailbox.remove(&self.base().id) {
+                    for msg in msgs {
+                        if let Some(message) = msg.downcast_ref::<<Self as ::alone_engine::GameObject>::Message>() {
+                            self.on_message(ctx, message);
+                        } else {
+                            println!("Tipo de evento incompatível recebido");
                         }
                     }
                 }
-                if ctx.mail_box_is_empty(){
+
+                if ctx.mail_box_is_empty() {
                     return;
                 }
                 #(self.#object_fields.dispatch_message(ctx);)*
             }
-            fn dispatch_event(&mut self, ctx: &mut impl ::alone_engine::EngineApi, event: &::alone_engine::GlobalEvent){
 
+            fn dispatch_event(&mut self, ctx: &mut impl ::alone_engine::EngineApi, event: &::alone_engine::GlobalEvent) {
                 #subscribe_block
                 #connect_block
                 #(self.#object_fields.dispatch_event(ctx, event);)*
             }
+
             fn dispatch_update(&mut self, ctx: &mut impl ::alone_engine::EngineApi, parent_base: &::alone_engine::Base, delta: f32) {
                 #apply_transform
-                if !self.is_started(){
+                if !self.is_started() {
                     self.dispatch_start(ctx, parent_base);
                     self.on_start();
                 }
                 self.update(ctx, delta);
-
                 #(self.#component_fields.update(ctx, &mut self.#base_field, delta);)*
-
-                #( self.#object_fields.dispatch_update(ctx, &self.#base_field, delta); )*
+                #(self.#object_fields.dispatch_update(ctx, &self.#base_field, delta);)*
             }
+
             fn dispatch_late_update(&mut self, ctx: &mut impl ::alone_engine::EngineApi, parent_base: &::alone_engine::Base, delta: f32) {
                 #apply_transform
-                if !self.is_started(){
+                if !self.is_started() {
                     self.dispatch_start(ctx, parent_base);
                     self.on_start();
                 }
                 self.late_update(ctx, delta);
-
                 #(self.#component_fields.late_update(ctx, &mut self.#base_field, delta);)*
-
-                #( self.#object_fields.dispatch_late_update(ctx, &self.#base_field, delta); )*
+                #(self.#object_fields.dispatch_late_update(ctx, &self.#base_field, delta);)*
             }
+
             fn dispatch_fixed_update(&mut self, ctx: &mut impl ::alone_engine::EngineApi, parent_base: &::alone_engine::Base, delta: f32) {
                 #apply_transform
-                if !self.is_started(){
+                if !self.is_started() {
                     self.dispatch_start(ctx, parent_base);
                     self.on_start();
                 }
                 self.fixed_update(ctx, delta);
-
                 #(self.#component_fields.fixed_update(ctx, &mut self.#base_field, delta);)*
-
-                #( self.#object_fields.dispatch_fixed_update(ctx, &self.#base_field, delta); )*
+                #(self.#object_fields.dispatch_fixed_update(ctx, &self.#base_field, delta);)*
             }
+
             fn dispatch_draw(&mut self, renderer: &mut impl ::alone_engine::RenderApi, parent_base: &::alone_engine::Base, blending: f32) {
                 #apply_transform
                 self.draw(renderer, blending);
-                
                 #(self.#component_fields.draw(renderer, &self.#base_field, blending);)*
-                #( self.#object_fields.dispatch_draw(renderer, &self.#base_field, blending); )*
-
+                #(self.#object_fields.dispatch_draw(renderer, &self.#base_field, blending);)*
             }
+
             fn dispatch_destroy(&mut self, ctx: &mut impl ::alone_engine::EngineApi) {
                 self.destroy(ctx);
                 #(self.#component_fields.destroy(ctx, &self.#base_field);)*
-                #( self.#object_fields.dispatch_destroy(ctx); )*
+                #(self.#object_fields.dispatch_destroy(ctx);)*
             }
         }
     }
@@ -267,17 +294,12 @@ pub fn scene_tree(input: TokenStream) -> TokenStream {
 }
 
 fn type_is_base(ty: &Type) -> bool {
-    match ty {
-        Type::Path(type_path) => {
-            if let Some(seg) = type_path.path.segments.last()
-                && seg.ident == "Base"
-            {
-                return true;
-            }
-            false
+    if let Type::Path(type_path) = ty {
+        if let Some(seg) = type_path.path.segments.last() {
+            return seg.ident == "Base";
         }
-        _ => false,
     }
+    false
 }
 
 #[proc_macro_derive(Scene)]
@@ -288,22 +310,21 @@ pub fn scene_dispatch_derive(input: TokenStream) -> TokenStream {
     let data = match input.data {
         syn::Data::Enum(d) => d,
         _ => {
-            return syn::Error::new_spanned(name, "Scene so pode ser utilizado em enum")
+            return syn::Error::new_spanned(name, "Scene só pode ser utilizado em enums")
                 .into_compile_error()
                 .into();
         }
     };
 
-    let variants: Vec<_> = data.variants.into_iter().map(|v| v.ident).collect();
+    let variants = data.variants.iter().map(|v| &v.ident);
 
     quote! {
         impl ::alone_engine::Scene for #name {
-            fn get_dispatch(&mut self) -> &mut impl ::alone_engine::GameObjectDispatch{
-                match self{
-                    #(Self::#variants(inner) => inner)*
+            fn get_dispatch(&mut self) -> &mut impl ::alone_engine::GameObjectDispatch {
+                match self {
+                    #(Self::#variants(inner) => inner,)*
                 }
             }
-
         }
     }
     .into()
